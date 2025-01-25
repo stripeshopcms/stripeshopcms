6547fa297820f06aa3617ece1f3879b2ff6479ce
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2025-01-25 11:53:23 -0600

    update

--- src/src/service/ProductService.ts
+++ src/src/service/ProductService.ts
@@ -1,6 +1,24 @@
 import { Request, Response } from "express"
-import { STRIPE_PUBLISHABLE_KEY } from "../../../config"
+import { STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY } from "../../../config"
 
-export function createProduct(req: Request, res: Response) {
-    res.send(STRIPE_PUBLISHABLE_KEY)
+const stripe = require("stripe")(STRIPE_SECRET_KEY)
+
+export async function createProduct(req: Request, res: Response) {
+    const product = await stripe.products.create({
+        name: "Second test product",
+        description: "This is a second test product.",
+        default_price_data: {
+            unit_amount: 1000,
+            currency: 'CAD'
+        }
+    })
+    const paymentLink = await stripe.paymentLinks.create({
+        line_items: [
+            {
+                price: product.default_price,
+                quantity: 1
+            }
+        ]
+    })
+    return res.json(paymentLink)
 }
